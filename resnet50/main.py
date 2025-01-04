5c73751255a93ead3c4adcc195738e035a5e3630
Omur <omur@erciyes.edu.tr>
2025-01-04 15:17:15 +0100

    new version of the tensorflow

--- resnet50/main.py
+++ resnet50/main.py
@@ -1,9 +1,11 @@
 import base64
 import io
+import os
+
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-from keras.src.applications.resnet import ResNet50, preprocess_input, decode_predictions
+from keras.applications.resnet import preprocess_input, decode_predictions, ResNet50
 from optiattack_client import collect_info
 
 # Base64 string'i çöz
@@ -14,7 +16,7 @@ def decode_base64_image(base64_string):
 
 # Görseli yükle ve ön işle
 def preprocess_image(image):
-    image = image.resize((224, 224))  # ResNet50 için uygun boyut
+    # image = image.resize((224, 224))  # ResNet50 için uygun boyut
     image_array = np.array(image)  # NumPy array'e dönüştür
     if image_array.shape[-1] == 4:  # RGBA ise RGB'ye dönüştür
         image_array = image_array[..., :3]
@@ -22,13 +24,13 @@ def preprocess_image(image):
     image_array = preprocess_input(image_array)  # ResNet50 preprocess işlemi
     return image_array
 
+NET_X = 100
+NET_Y = 100
 
 @collect_info()
-def resnet50_main(file):
-
-    image = decode_base64_image(file)
-
-    processed_image = preprocess_image(image)
+def resnet50_main(data):
+    image_array = np.array(data, dtype=np.uint8)
+    processed_image = preprocess_image(image_array)
 
     predictions = model.predict(processed_image)
     decoded_predictions = decode_predictions(predictions, top=10)
@@ -39,7 +41,29 @@ def resnet50_main(file):
 
     return {"predictions": json_results}
 
-model = ResNet50(weights="imagenet")
+os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+gpus = tf.config.experimental.list_physical_devices("GPU")
 
+for gpu in gpus:
+    try:
+        tf.config.experimental.set_virtual_device_configuration(
+            gpu,
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=8192)])
+    except RuntimeError as e:
+        print(e)
+
+logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+
+model = ResNet50(
+    include_top=True,
+    weights='imagenet',
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=1000,
+    classifier_activation='softmax'
+)
 while True:
     pass
