aff6bbd1e57453b5beaa58f43b08c58c2a2b82e3
ekilic <ersinkilic@erciyes.edu.tr>
2025-02-03 20:59:58 +0300

    onnxruntime

--- onnxruntime/main.py
+++ onnxruntime/main.py
@@ -1,40 +1,28 @@
 import onnxruntime as ort
 import numpy as np
-import cv2
 import requests
+from optiattack_client import collect_info
 
 
-# ONNX model yolunu belirtin
-model_path = "onnxruntime.onnx"
+def softmax(x, axis):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=axis)
 
-# Sınıf isimlerini yükle
+model_path = "resnet50-v2-7.onnx"
+
+# classes
 url = "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"
 classes = [line.strip() for line in requests.get(url).text.split("\n") if line]
 
-def preprocess_image(image_path):
-    """Görüntüyü yükler ve ONNX için uygun bir tensöre dönüştürür."""
-    # Görüntüyü yükle
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # BGR'den RGB'ye çevir
-
-    # Boyutlandırma ve kırpma
-    h, w, _ = image.shape
-    scale = 256 / min(h, w)
-    resized_h, resized_w = int(h * scale), int(w * scale)
-    image = cv2.resize(image, (resized_w, resized_h), interpolation=cv2.INTER_AREA)
-
-    # Merkezden kırpma
-    start_x = (resized_w - 224) // 2
-    start_y = (resized_h - 224) // 2
-    image = image[start_y:start_y + 224, start_x:start_x + 224]
-
-    # Normalizasyon
+def preprocess_image(image):
+    image = np.reshape(image, (224,224,3))
+    # Normalization
     image = image.astype(np.float32) / 255.0
     mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
     std = np.array([0.229, 0.224, 0.225], dtype=np.float32)
     image = (image - mean) / std
 
-    # Kanal sırasını değiştirme ve batch boyutunu ekleme
     image = np.transpose(image, (2, 0, 1))  # HWC -> CHW
     image = np.expand_dims(image, axis=0)  # (C, H, W) -> (1, C, H, W)
 
@@ -45,14 +33,15 @@ input_name = session.get_inputs()[0].name
 @collect_info()
 def run_inference(data):
     input_tensor = preprocess_image(data)
-    # Çıkarımı çalıştır
+    # inference
     outputs = session.run(None, {input_name: input_tensor})
-
-    # Çıktı sınıfını belirle
     output = outputs[0]
-    score = float(outputs[1])
-    predicted_class = np.argmax(output)
+    output = softmax(output, 1)
+    indices = np.argsort(output, axis=1)[:,-5:]
     json_results = []
-    json_results.append({"label": predicted_class, "score": score})
+    for i in indices[0][::-1]:
+        json_results.append({"label": classes[i], "score": float(output[0, i])})
     return {"predictions": json_results}
 
+while True:
+    pass
